d260f32b394aceaf7483651d0b4c8be06f81209a
Sebastián Ayala <52267585+asar1245@users.noreply.github.com>
2021-03-27 15:58:41 -0500

    Add files via upload

---
+++ Sesion9_loops/archivos/forloop.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# File: printrectangle.sh
+echo "Ingrese el valor de altura: "
+read m
+echo "Ingrese el valor de base: "
+read n
+for((i=0; i<m; i=i+1))
+do
+    for((j=0; j<n; j=j+1))
+    do
+        if (( i==0 )) || (( i==m-1 )) || (( j==0 )) || (( j==n-1 ))
+        then
+            echo -n "*"
+        else
+            echo -n " "
+        fi  
+    done
+    printf "\n"
+done
+
+
